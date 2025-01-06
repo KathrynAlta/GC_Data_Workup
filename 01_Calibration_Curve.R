3ac31b48db1b7285db0e607dcd2dde78b8c8f5f6
Your Name <kgannon59401@gmail.com>
2025-01-06 13:24:36 -0700

    set diff wds

--- 01_Calibration_Curve.R
+++ 01_Calibration_Curve.R
@@ -3,8 +3,9 @@
 # 00. Set Up R Environment ----
 
     # Set working directory 
-    setwd("/Users/altagannon/Library/CloudStorage/OneDrive-UCB-O365/Graduate_School/05_Research_Projects/02_GHG_Under_Ice")
-    
+    # setwd("/Users/altagannon/Library/CloudStorage/OneDrive-UCB-O365/Graduate_School/05_Research_Projects/02_GHG_Under_Ice") # Laptop KAG
+    setwd("/Users/kaga3666/Library/CloudStorage/OneDrive-UCB-O365/Graduate_School/05_Research_Projects/02_GHG_Under_Ice")    # Desktop SEEC
+
     # Load packages and functions 
     source("02_Analysis/GC_Data_Workup/00_libraries.R")
     source("02_Analysis/GC_Data_Workup/00_functions.R")
